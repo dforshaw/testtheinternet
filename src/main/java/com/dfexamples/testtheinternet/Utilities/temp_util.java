754f00b8fd86f96c52cbdb4f7298140a7a7e88da
David Forshaw <dfcv126@gmail.com>
2016-12-18 15:27:17 -0800

    temp - placeholder file

--- src/main/java/com/dfexamples/testtheinternet/Utilities/temp_util.java
+++ src/main/java/com/dfexamples/testtheinternet/Utilities/temp_util.java
@@ -1,11 +1,4 @@
 package com.dfexamples.testtheinternet.Utilities;
 
-/**
- * Created with IntelliJ IDEA.
- * User: David Forshaw
- * Date: 12/18/2016
- * Time: 3:25 PM
- */
-
 public class temp_util {
 }
