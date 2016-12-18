7ce616c819ce6a5658f2dc8989c96e6ebcc47f7b
David Forshaw <dfcv126@gmail.com>
2016-12-18 15:17:44 -0800

    Added Firefox config to testcase

--- src/test/java/com/dfexamples/testtheinternet/LoginTests/FirstLoginTest.java
+++ src/test/java/com/dfexamples/testtheinternet/LoginTests/FirstLoginTest.java
@@ -8,10 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstLoginTest {
 
+    public static String userHomeDir = System.getProperty("user.home");
+    public static String Webdriver_Clients = userHomeDir + "/proj/webdriver_clients";
+
     @Test
     public void basicTest() {
+        String fireFoxDriverLocation = Webdriver_Clients + "/geckodriver/geckodriver";
+        System.setProperty("webdriver.gecko.driver", fireFoxDriverLocation);
         WebDriver driver = new FirefoxDriver();
+
         driver.get("http://the-internet.herokuapp.com/");
-        assertThat(driver.getTitle()).isEqualToIgnoringCase("The Internet");
+        assertThat(driver.getTitle()).isEqualToIgnoringCase("The Internet1");
+
+        driver.close();
+        driver.quit();
     }
 }
