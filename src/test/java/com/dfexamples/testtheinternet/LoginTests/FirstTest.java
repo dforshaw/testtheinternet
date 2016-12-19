d702444f5afd37afffe9c9a12fcb272f61cdfe57
David Forshaw <dfcv126@gmail.com>
2016-12-18 16:20:34 -0800

    Refactoring for added base test

--- src/test/java/com/dfexamples/testtheinternet/LoginTests/FirstTest.java
+++ src/test/java/com/dfexamples/testtheinternet/LoginTests/FirstTest.java
@@ -1,14 +1,14 @@
 package com.dfexamples.testtheinternet.LoginTests;
 
+import com.dfexamples.testtheinternet.BaseTest;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FirstTest {
+public class FirstTest extends BaseTest{
 
     public static String userHomeDir = System.getProperty("user.home");
     public static String projHomeDir = userHomeDir + "/Ideaprojects/testtheinternet";
@@ -18,13 +18,11 @@ public class FirstTest {
     public void basicFirefoxTest() {
         String fireFoxDriverLocation = Webdriver_Clients + "/geckodriver/geckodriver";
         System.setProperty("webdriver.gecko.driver", fireFoxDriverLocation);
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
 
         driver.get("http://the-internet.herokuapp.com/");
         assertThat(driver.getTitle()).isEqualToIgnoringCase("The Internet");
-
         driver.close();
-        driver.quit();
     }
 
     @Test
@@ -33,25 +31,21 @@ public class FirstTest {
         capabilities.setCapability("marionette", true);
         String marionetteDriverLocation = Webdriver_Clients + "/geckodriver/geckodriver";
         System.setProperty("webdriver.gecko.driver", marionetteDriverLocation);
-        WebDriver driver = new FirefoxDriver(capabilities);
+        driver = new FirefoxDriver(capabilities);
 
         driver.get("http://the-internet.herokuapp.com/");
         assertThat(driver.getTitle()).isEqualToIgnoringCase("The Internet");
-
         driver.close();
-        driver.quit();
     }
 
     @Test
     public void basicChromeTest() {
         String chromeDriverLocation = Webdriver_Clients + "/chromedriver/chromedriver";
         System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         driver.get("http://the-internet.herokuapp.com/");
         assertThat(driver.getTitle()).isEqualToIgnoringCase("The Internet");
-
         driver.close();
-        driver.quit();
     }
 }
